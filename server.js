1ce9994870ae138351d4bde4358dcf268a321ee3
Sarah Ronald <sareronald@hotmail.com>
2020-12-09 22:24:17 +1100

    setting up server.js and routes

---
+++ server.js
@@ -0,0 +1,50 @@
+// require dependencies
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+//requires the content in the models folder
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// host static files so CSS and js files can be retreived
+app.use(express.static("public"));
+
+// Connecting to Mongo Atlas: workoutdb database
+const PWD = "";
+const databaseUrl = `mongodb+srv://sareronald:${encodeURIComponent(
+  PWD
+)}@primarycluster0.sxdap.mongodb.net/workoutdb`;
+
+mongoose.connect(databaseUrl, { useNewUrlParser: true });
+
+// Routes
+// ================
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "stats.html"));
+});
+
+// Middleware
+// ================
+
+// Start the Server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
